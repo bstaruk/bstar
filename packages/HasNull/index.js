fa7d870346d3f06fa602239f1415a948d197c2d7
Brian Staruk <brian@staruk.me>
2018-12-17 08:52:20 -0500

    hasnull 0.0.4 attempt

--- packages/HasNull/index.js
+++ packages/HasNull/index.js
@@ -1,11 +1,9 @@
 // checks if any value in an object is null
-const HasNull = (obj) => {
-  const objKeys = Object.keys(obj);
+function HasNull (obj) {
+  var objKeys = Object.keys(obj);
   for (let i = 0; i < objKeys.length; i++) {
     if (obj[objKeys[i]] == null) { return true; }
   }
 
   return false;
-};
-
-export default HasNull;
+}
