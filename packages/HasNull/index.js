a838098075e8816190488ce8a364fc1662ba5e00
Brian Staruk <brian@staruk.me>
2018-12-18 08:27:09 -0500

    hasnull - removing webpack (just use babel)

--- packages/HasNull/index.js
+++ packages/HasNull/index.js
@@ -1,81 +1,23 @@
-(function(modules) {
-    var installedModules = {};
-    function __webpack_require__(moduleId) {
-        if (installedModules[moduleId]) {
-            return installedModules[moduleId].exports;
-        }
-        var module = installedModules[moduleId] = {
-            i: moduleId,
-            l: false,
-            exports: {}
-        };
-        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-        module.l = true;
-        return module.exports;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+// checks if any value in an object is null
+var HasNull = function HasNull(obj) {
+  var objKeys = Object.keys(obj);
+  var i;
+
+  for (i = 0; i < objKeys.length; i += 1) {
+    if (obj[objKeys[i]] == null) {
+      return true;
     }
-    __webpack_require__.m = modules;
-    __webpack_require__.c = installedModules;
-    __webpack_require__.d = function(exports, name, getter) {
-        if (!__webpack_require__.o(exports, name)) {
-            Object.defineProperty(exports, name, {
-                enumerable: true,
-                get: getter
-            });
-        }
-    };
-    __webpack_require__.r = function(exports) {
-        if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
-            Object.defineProperty(exports, Symbol.toStringTag, {
-                value: "Module"
-            });
-        }
-        Object.defineProperty(exports, "__esModule", {
-            value: true
-        });
-    };
-    __webpack_require__.t = function(value, mode) {
-        if (mode & 1) value = __webpack_require__(value);
-        if (mode & 8) return value;
-        if (mode & 4 && typeof value === "object" && value && value.__esModule) return value;
-        var ns = Object.create(null);
-        __webpack_require__.r(ns);
-        Object.defineProperty(ns, "default", {
-            enumerable: true,
-            value: value
-        });
-        if (mode & 2 && typeof value != "string") for (var key in value) __webpack_require__.d(ns, key, function(key) {
-            return value[key];
-        }.bind(null, key));
-        return ns;
-    };
-    __webpack_require__.n = function(module) {
-        var getter = module && module.__esModule ? function getDefault() {
-            return module["default"];
-        } : function getModuleExports() {
-            return module;
-        };
-        __webpack_require__.d(getter, "a", getter);
-        return getter;
-    };
-    __webpack_require__.o = function(object, property) {
-        return Object.prototype.hasOwnProperty.call(object, property);
-    };
-    __webpack_require__.p = "";
-    return __webpack_require__(__webpack_require__.s = 0);
-})([ function(module, exports, __webpack_require__) {
-    module.exports = __webpack_require__(1);
-}, function(module, __webpack_exports__, __webpack_require__) {
-    "use strict";
-    __webpack_require__.r(__webpack_exports__);
-    var HasNull = function HasNull(obj) {
-        var objKeys = Object.keys(obj);
-        var i;
-        for (i = 0; i < objKeys.length; i += 1) {
-            if (obj[objKeys[i]] == null) {
-                return true;
-            }
-        }
-        return false;
-    };
-    __webpack_exports__["default"] = HasNull;
-} ]);
+  }
+
+  return false;
+};
+
+var _default = HasNull;
+exports.default = _default;
